b6c74ac069116f759ab39872001b4634212ef427
Raafat Elmenayar <raafat.el123@gmail.com>
2023-07-30 00:02:54 +0300

    Update profile_screen.dart

--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iti_graduation_project/utilities/app_colors.dart';
+import 'package:iti_graduation_project/view/screens/welcome_screen.dart';
+import '../../utilities/google_signin.dart';
 
 class ProfileScreen extends StatefulWidget {
   ProfileScreen({super.key});
@@ -15,7 +17,7 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   User? user = FirebaseAuth.instance.currentUser;
-  
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -28,16 +30,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               //Stack فيها شكل الستوري وصورة لبروفايل
-              const CircleAvatar(
+              CircleAvatar(
                 radius: 65,
-                backgroundImage: NetworkImage(
-                    "https://th.bing.com/th?id=OIP.1f4L8uI7SgYFrlQ1taL1YgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"),
+                backgroundImage: NetworkImage(user?.photoURL != null
+                    ? user!.photoURL as String
+                    : "https://th.bing.com/th?id=OIP.1f4L8uI7SgYFrlQ1taL1YgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"),
               ),
               const SizedBox(height: 20),
               //الاسم
-              const Text(
-                "Username",
-                style: TextStyle(
+              Text(
+                user!.displayName.toString(),
+                style: const TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
                     color: AppColors.text),
@@ -64,7 +67,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   padding:
                       const EdgeInsets.symmetric(vertical: 12, horizontal: 25),
                   child: Text(
-                    user != null ? user!.displayName as String : "first name",
+                    user!.email as String,
                     style: const TextStyle(
                       fontSize: 19,
                       color: AppColors.text,
@@ -73,6 +76,49 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
               const SizedBox(height: 27),
+              user?.emailVerified == false
+                  ? Column(
+                    children: [
+                      const Text("Your email is not verified",
+                        style: TextStyle(color: AppColors.text, fontSize: 16),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            user?.sendEmailVerification();
+                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              backgroundColor: AppColors.background,
+                              content: Text(
+                                "Verification email sent",
+                                style: TextStyle(color: AppColors.text),
+                              ),
+                            ));
+                            },
+                          child: const Text("send verification email",
+                              style: TextStyle(fontSize: 16)),),
+                      const SizedBox(height: 20,)
+                    ],
+                  )
+                  : const SizedBox(),
+              TextButton(
+                  onPressed: () async {
+                    googleSignOut();
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WelcomeScreen(),
+                      ),
+                          (_) => false,
+                    );
+                  },
+                  child: const Text("Sign Out",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+              ),
             ],
           ),
         ),
